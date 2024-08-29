99c16ca3fd2dece7dadbb422a0d21ef984357a49
Marcus Schmidke <ms@mtrail.ch>
2024-08-29 17:17:42 +0200

    Souvenirshop anlegen
    $t/05_t
    $t/06_b

--- src/main/java/Souvenirshop.java
+++ src/main/java/Souvenirshop.java
@@ -1,3 +1,34 @@
+import java.util.List;
+
 public class Souvenirshop {
 
+  private final String name;
+  private final List<String> artikel;
+  private final double preis;
+
+  public Souvenirshop(final String name, final List<String> artikel, final double preis) {
+    this.name = name;
+    this.artikel = artikel;
+    this.preis = preis;
+  }
+
+  // Getter und Setter
+  public String getName() {
+    return name;
+  }
+
+  public List<String> getArtikel() {
+    return artikel;
+  }
+
+  public double getPreis() {
+    return preis;
+  }
+
+  public void zeigeAngebote() {
+    System.out.println("Im " + name + " gibt es folgende Souvenirs:");
+    for (String artikel : artikel) {
+      System.out.println("- " + artikel + " für " + preis + " Euro");
+    }
+  }
 }
