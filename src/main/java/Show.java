bcac008a658793db6754ecb504df54fd310dad2a
Marcus Schmidke <ms@mtrail.ch>
2024-08-29 17:07:58 +0200

    Show: Füge Zeit hinzu.
    $t/04_t
    $t/05_b

--- src/main/java/Show.java
+++ src/main/java/Show.java
@@ -1,17 +1,23 @@
 public class Show {
 
   private final String titel;
+  private final String zeit; // z.B. "15:00 Uhr"
 
-  public Show(final String titel) {
+  public Show(final String titel, final String zeit) {
     this.titel = titel;
+    this.zeit = zeit;
   }
 
   public String getTitel() {
     return titel;
   }
 
+  public String getZeit() {
+    return zeit;
+  }
+
   @Override
   public String toString() {
-    return "Show " + titel;
+    return "Show " + titel + " findet um " + zeit + " statt.";
   }
 }
