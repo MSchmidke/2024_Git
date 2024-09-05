0d8286fa2e5116b3bc390704171e33b02fd7505c
Marcus Schmidke <ms@mtrail.ch>
2024-09-05 16:32:47 +0200

    Füge Klasse Ersatzteil hinzu

---
+++ src/main/java/Ersatzteil.java
@@ -0,0 +1,16 @@
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+@RequiredArgsConstructor
+@Getter
+@Builder
+@ToString
+public class Ersatzteil {
+
+  private final int id;
+  private final String name;
+  private final String bestellnummer;
+
+}
