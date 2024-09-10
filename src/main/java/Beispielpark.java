c9002dddb09ca299601dda12746ebd943f59af33
Marcus Schmidke <ms@mtrail.ch>
2024-09-10 17:08:45 +0200

    Weitere Familienmitglieder
    $t/09_d

--- src/main/java/Beispielpark.java
+++ src/main/java/Beispielpark.java
@@ -1,9 +1,13 @@
+import java.util.List;
+
 public class Beispielpark {
 
   public static void main(final String[] args) {
     var park = new Freizeitpark("Phantasialand");
     System.out.println(park);
     var familie = List.of(//
+        new Parkbesucher("Hans Müller", 45, 180, true), //
+        new Parkbesucher("Anna Müller", 42, 165, true), //
         new Parkbesucher("Lukas Müller", 15, 160, true), //
         new Parkbesucher("Sophie Müller", 12, 150, true));
     System.out.println(familie);
