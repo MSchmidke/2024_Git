0c2be2a7c74d47bc0b041feb1b79d059618c2cc7
Marcus Schmidke <ms@mtrail.ch>
2024-09-10 15:42:19 +0200

    Show: Füge Ort hinzu.
    $t/24_b

--- src/main/java/Show.java
+++ src/main/java/Show.java
@@ -1,17 +1,23 @@
 public class Show {
 
   private final String titel;
+  private final String ort;
 
-  public Show(final String titel) {
+  public Show(final String titel, final String ort) {
     this.titel = titel;
+    this.ort = ort;
   }
 
   public String getTitel() {
     return titel;
   }
 
+  public String getOrt() {
+    return ort;
+  }
+
   @Override
   public String toString() {
-    return "Show " + titel;
+    return "Show " + titel + " findet im " + ort + " statt.";
   }
 }
