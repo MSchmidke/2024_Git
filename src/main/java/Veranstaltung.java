cbc728dfe9c74f394ef3ebbfa9baec6d0863fccb
Marcus Schmidke <ms@mtrail.ch>
2024-09-11 16:29:24 +0200

    Klasse Veranstaltung - Teil 2
    $t/10_b

--- src/main/java/Veranstaltung.java
+++ src/main/java/Veranstaltung.java
@@ -1,3 +1,13 @@
 public class Veranstaltung {
 
+  private final String name;
+
+  public Veranstaltung(final String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
 }
