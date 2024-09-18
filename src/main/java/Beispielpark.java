c985c2ccd7314326bddfe1b629223d18ccc8f129
Marcus Schmidke <ms@mtrail.ch>
2024-09-18 18:02:35 +0200

    Füge Tim, Anna, Leon hinzu

--- src/main/java/Beispielpark.java
+++ src/main/java/Beispielpark.java
@@ -1,8 +1,16 @@
+import java.util.List;
+
 public class Beispielpark {
 
   public static void main(final String[] args) {
     var park = new Freizeitpark("Phantasialand");
+    var besucher = List.of(//
+        new Parkbesucher("Tim", 22, 175, true)//
+        , new Parkbesucher("Anna", 28, 160, true)//
+        , new Parkbesucher("Leon", 35, 185, false)//
+    );
     System.out.println(park);
+    System.out.println(besucher);
   }
 
 }
