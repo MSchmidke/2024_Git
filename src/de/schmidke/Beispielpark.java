b3f2fb1bb103334a250aae783bbd0b081bf1ca9f
Marcus Schmidke <ms@mtrail.ch>
2024-09-26 18:09:54 +0200

    Fix Name
    $t/36_main

--- src/de/schmidke/Beispielpark.java
+++ src/de/schmidke/Beispielpark.java
@@ -1,7 +1,7 @@
 public class Beispielpark {
 
   public static void main(final String[] args) {
-    var park = new Freizeitpark("Phantasialand");
+    var park = new Freizeitpark("Hansa-Park");
     System.out.println(park);
   }
 
