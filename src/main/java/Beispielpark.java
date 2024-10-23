609a95c7ee2c2f6ebe0100d7f90cd1e71bef785d
Marcus Schmidke <ms@mtrail.ch>
2024-10-23 15:48:42 +0200

    Füge Beispile für Attraktionen hinzu
    $b/01_branch
    $b/03
    $t/09_e

--- src/main/java/Beispielpark.java
+++ src/main/java/Beispielpark.java
@@ -4,6 +4,13 @@ public class Beispielpark {
 
   public static void main(final String[] args) {
     var park = new Freizeitpark("Phantasialand");
+    park.fügeAttraktionHinzu(new Attraktion("Taron", "Achterbahn", 130, 28));
+    park.fügeAttraktionHinzu(new Attraktion("Black Mamba", "Achterbahn", 140, 28));
+    park.fügeAttraktionHinzu(new Attraktion("Chiapas", "Wildwasserbahn", 120, 40));
+    park.fügeAttraktionHinzu(new Attraktion("Raik", "Familien-Achterbahn", 100, 20));
+    park.fügeAttraktionHinzu(new Attraktion("Mystery Castle", "Drop Tower", 130, 20));
+    park.fügeAttraktionHinzu(new Attraktion("Crazy Bats", "VR-Achterbahn", 100, 18));
+    park.fügeAttraktionHinzu(new Attraktion("Wakobato", "Wasserfahrt", 0, 30));
     System.out.println(park);
     var familie = List.of(//
         new Parkbesucher("Hans Müller", 45, 180, true), //
