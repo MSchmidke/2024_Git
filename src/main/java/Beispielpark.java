fa6682e738647488760e058b19fa3b5f14f2df93
Marcus Schmidke <ms@mtrail.ch>
2024-10-23 15:48:25 +0200

    Füge eine Besucherfamilie hinzu
    $b/02
    $b/02_2
    $t/01_t
    $t/02_d
    $t/09_c

--- src/main/java/Beispielpark.java
+++ src/main/java/Beispielpark.java
@@ -3,6 +3,10 @@ public class Beispielpark {
   public static void main(final String[] args) {
     var park = new Freizeitpark("Phantasialand");
     System.out.println(park);
+    var familie = List.of(//
+        new Parkbesucher("Lukas Müller", 15, 160, true), //
+        new Parkbesucher("Sophie Müller", 12, 150, true));
+    System.out.println(familie);
   }
 
 }
